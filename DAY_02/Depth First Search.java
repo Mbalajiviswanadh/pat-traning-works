4fb7e3d51d97ab84243fdbba5d712a860ba552e9
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-04-05 16:18:55 +0530

    DFS

---
+++ DAY_02/Depth First Search.java
@@ -0,0 +1,39 @@
+package DAY_02;
+
+import java.util.*;
+
+ class DFS {
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        graph = new ArrayList<>(n);
+        visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            graph.get(u).add(v);
+            graph.get(v).add(u); 
+        }
+        dfs(0);
+        
+        scanner.close();
+    }
+    static void dfs(int node) {
+        visited[node] = true;
+        System.out.print(node + " ");
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor);
+            }
+        }
+    }
+}
+
