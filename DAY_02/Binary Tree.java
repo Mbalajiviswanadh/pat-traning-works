1a4499455671f6c1cf5c395c4ae5751410ad1862
balajiviswanadh <101652480+Mbalajiviswanadh@users.noreply.github.com>
2024-04-05 10:36:27 +0530

    in order and level order `

--- DAY_02/Binary Tree.java
+++ DAY_02/Binary Tree.java
@@ -1,38 +1,60 @@
 package DAY_02;
 import java.util.*;
-class treeNode{
-    int data;
-    treeNode left,right;
-    treeNode(int n){
-        this.data=n;
-    }
 
-    public static void inorder(treeNode curr){
-        if(curr==null){
+class treeNode {
+    int data;
+    treeNode left, right;
+
+    treeNode(int n) {
+        this.data = n;
+    }
+}
+
+class tree {
+
+    treeNode root;
+
+    // in order traversal
+    public void inorder(treeNode curr) {
+        if (curr == null) {
             return;
         }
         inorder(curr.left);
-        System.out.print(curr.data+" ");
+        System.out.print(curr.data + " ");
         inorder(curr.right);
     }
-    
-}
 
+    public void levelOrder() {
+        Queue<treeNode> q = new LinkedList<treeNode>();
 
-
-class demo{
-    public static void main(String[] args) {
-        treeNode root=new treeNode(1);
-        root.left=new treeNode(5);
-        root.right=new treeNode(6);
-        root.left.left=new treeNode(2);
-        root.left.right=new treeNode(7);
-        root.right.left=new treeNode(8);
-        root.right.right=new treeNode(9);
-        
-
-        root.inorder(root);
+        q.offer(root);
+        while (!q.isEmpty()) {
+            treeNode curr = q.poll();
+            System.out.print(curr.data + " ");
+            if (curr.left != null) {
+                q.add(curr.left);
+            }
+            if (curr.right != null) {
+                q.add(curr.right);
+            }
+        }
     }
 }
 
+class demo {
+    public static void main(String[] args) {
+        tree t = new tree();
+        t.root = new treeNode(1);
+        t.root.left = new treeNode(5);
+        t.root.right = new treeNode(6);
+        t.root.left.left = new treeNode(2);
+        t.root.left.right = new treeNode(7);
+        t.root.right.left = new treeNode(8);
+        t.root.right.right = new treeNode(9);
+
+        t.inorder(t.root); 
+        System.out.println();
+        t.levelOrder();
+    }
+}
 
